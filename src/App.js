526b1c4afa50b23203fb26a170920ae9a8d4e10f
Mouhammadou Oury Diallo <ryhow99@gmail.com>
2021-09-11 17:54:04 +0000

    with routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import addmovie from "./redux/movies/action";
-
+import { Route , Switch, Link} from "react-router-dom";
+import Description from "./Components/Description";
 
 function App() {
-
   const dispatch = useDispatch();
 
   const [vtitle, setVtitle] = useState(" ");
@@ -17,8 +17,7 @@ function App() {
 
   const [newMovie, setNewMovie] = useState();
 
-
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     return dispatch(addmovie(newMovie));
   }
@@ -35,86 +34,90 @@ function App() {
   console.log(newMovie);
   return (
     <div className="App container">
-      <h1>Movie App</h1>
-      <h4 style={{ color: "white" }}>
-        I make this app to collect and store my favorite movies.
-        <br />
-        Let's see it.
-      </h4>
+      <Link to='/'>Home</Link>
+      <Switch>
+        <Route exact path="/">
+          <h1>Movie App</h1>
+          <h4 style={{ color: "white" }}>
+            I make this app to collect and store my favorite movies.
+            <br />
+            Let's see it.
+          </h4>
 
-      <form className="container m-3" onSubmit={(e) => handleSubmit(e)}>
-        <div className="form-group">
-          <label htmlFor="title" className="text-white">
-            Title:
-          </label>
-          <input
-            type="text"
-            onChange={(e) => setVtitle(e.target.value)}
-            className="form-control"
-            placeholder="Enter title"
-            id="title"
-            name="title"
-            value={vtitle}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="description" className="text-white">
-            Description:
-          </label>
-          <input
-            type="text"
-            onChange={(e) => setVdescription(e.target.value)}
-            className="form-control"
-            placeholder="Enter description"
-            id="description"
-            name="description"
-            value={vdescription}
-            required
-          />
-        </div>
+          <form className="container m-3" onSubmit={(e) => handleSubmit(e)}>
+            <div className="form-group">
+              <label htmlFor="title" className="text-white">
+                Title:
+              </label>
+              <input
+                type="text"
+                onChange={(e) => setVtitle(e.target.value)}
+                className="form-control"
+                placeholder="Enter title"
+                id="title"
+                name="title"
+                value={vtitle}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description" className="text-white">
+                Description:
+              </label>
+              <input
+                type="text"
+                onChange={(e) => setVdescription(e.target.value)}
+                className="form-control"
+                placeholder="Enter description"
+                id="description"
+                name="description"
+                value={vdescription}
+                required
+              />
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="posterUrl" className="text-white">
-            Poster-Url:
-          </label>
-          <input
-            type="text"
-            onChange={(e) => setVposterUrl(e.target.value)}
-            className="form-control"
-            placeholder="Paste the url of the image here"
-            id="posterUrl"
-            name="posterUrl"
-            value={vposterUrl}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="rating" className="text-white">
-            Rating:
-          </label>
-          <input
-            type="number"
-            onChange={(e) => setVrating(e.target.value)}
-            className="form-control"
-            placeholder="Give the rate"
-            id="email"
-            name="rating"
-            value={vrating}
-            required
-          />
-        </div>
+            <div className="form-group">
+              <label htmlFor="posterUrl" className="text-white">
+                Poster-Url:
+              </label>
+              <input
+                type="text"
+                onChange={(e) => setVposterUrl(e.target.value)}
+                className="form-control"
+                placeholder="Paste the url of the image here"
+                id="posterUrl"
+                name="posterUrl"
+                value={vposterUrl}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="rating" className="text-white">
+                Rating:
+              </label>
+              <input
+                type="number"
+                onChange={(e) => setVrating(e.target.value)}
+                className="form-control"
+                placeholder="Give the rate"
+                id="email"
+                name="rating"
+                value={vrating}
+                required
+              />
+            </div>
 
-        <button type="submit" className="btn btn-primary">
-          Add Film
-        </button>
-      </form>
+            <button type="submit" className="btn btn-primary">
+              Add Film
+            </button>
+          </form>
 
-      <MovieList />
+          <MovieList />
+        </Route>
+        <Route path="/:title" component={Description}/>
+      </Switch>
     </div>
   );
-
-  
 }
 
 export default App;
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function MovieCard({title , description , posterUrl, rating}){
-
-    return(
-        <div className='m-4'>
-            <img src={posterUrl} alt='film' width="300px" height="200px"/>
-            <h6>Title: {title}</h6>
-            <h6>Description : {description}</h6>
-            <h6>Rating : {rating}</h6>
-        </div>
-    );
-
+function MovieCard({ title, description, posterUrl, rating }) {
+  return (
+    <div className="m-4">
+      <Link to={`/${title}`} >
+        <img src={posterUrl} alt="film" width="300px" height="200px" />
+        <h6>Title: {title}</h6>
+        <h6>Description : {description}</h6>
+        <h6>Rating : {rating}</h6>
+      </Link>
+    </div>
+  );
 }
 
 export default MovieCard;
